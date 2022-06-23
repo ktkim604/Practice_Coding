db88aaa6af07771c256d21fef5e23771162a3b93
Kyu Tae Kim <104997803+ktkim604@users.noreply.github.com>
2022-06-24 00:04:16 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -31,15 +31,54 @@
 # print("Red Apple\rPine")
 
 #퀴즈
-url = "http://naver.com"
-my_str = url.replace("http://","")
-print(my_str)
-print(url[7:])
-my_str = my_str[:my_str.index(".")]
-print(my_str)
+# url = "http://naver.com"
+# my_str = url.replace("http://","")
+# print(my_str)
+# print(url[7:])
+# my_str = my_str[:my_str.index(".")]
+# print(my_str)
 
-password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
-print("{0} ")
+# password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+# print("{0} ")
+
+#리스트
+# subway = ["유재석", "조세호", "박명수"]
+# print(subway)
+
+# subway.append("하하")
+# print(subway)
+
+# subway.insert(1, "정형돈")
+# print(subway)
+
+# # 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop(1))
+# print(subway)
+
+# subway.append("")
+
+# import unicodedata
+
+# string = '인코딩의 블로그'
+
+# uni1 = unicodedata.normalize('NFD',string)
+# print(uni1)
+
+# uni2 = unicodedata.normalize('NFC',uni1)
+# print(uni2)
+
+from random import sample, shuffle
 
 
+users = range(1,21)
+#print(type(users))
+users = list(users)
+#print(users)
+shuffle(users)
+winners = sample(users, 4)
 
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("음료 당첨자 : {0}".format(winners[1:]))
